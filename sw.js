8708a64a481004e926b6709763e854328b10aae3
Citizen_97 <137045169+FaraPatton@users.noreply.github.com>
2026-05-22 11:16:53 +0300

    Change cache version to v7
    Updated cache version from v5 to v7.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — ИП Карпов Docs PWA
 // Версия кэша — меняй при каждом обновлении index.html
-const CACHE = 'karpov-docs-v5';
+const CACHE = 'karpov-docs-v7';
 
 // Файлы для кэширования при установке
 const PRECACHE = [
